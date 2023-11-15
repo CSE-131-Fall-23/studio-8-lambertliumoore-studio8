af9ade4fb90ff1bf79b4b473ffa803455f9fc455
spencerlambert <spencerlambert@localhost>
2023-11-15 14:17:26 -0600

    studio

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -1,18 +1,31 @@
 package studio8;
 
+import java.util.Scanner;
+
 public class MultipleChoiceQuestion extends Question {
 	
+	
+	
+	private String[] choices;
+
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
 		//FIXME
-		
+		super(prompt,answer,points);
+		this.choices=choices;
 		//Call the super class constructor, then create and set
 		//instance variables for any values that aren't handled
 		//by the base class
 	}
 	
+	
+	
 	public void displayPrompt() {
 		//FIXME
-		
+		System.out.println(this.getPrompt());
+		for(int i=0; i<this.choices.length;i++) {
+			System.out.print((i+1)+":");
+			System.out.println(this.choices[i]);
+		}
 		/*
 		 * Use the method from the base class to display the prompt,
 		 * Then write more code to display all of the choices like so:
@@ -30,9 +43,20 @@ public class MultipleChoiceQuestion extends Question {
 		String[] choices = {"seven", "nine", "eight", "six"};
 		Question multipleChoice = new MultipleChoiceQuestion("What studio is this?", "3", 1, choices);
 		multipleChoice.displayPrompt();
-		System.out.println(multipleChoice.checkAnswer("hi"));//wrong
-		System.out.println(multipleChoice.checkAnswer("1"));//wrong
-		System.out.println(multipleChoice.checkAnswer("3"));//right
+		Scanner in = new Scanner (System.in);
+		System.out.println("What is your answer?");
+		String selected=in.next();
+		
+		
+		if(multipleChoice.checkAnswer(selected)==1) {
+			System.out.println("You are correct");
+		} else {
+			System.out.println("You are incorrect");
+		}
+		
+		//System.out.println(multipleChoice.checkAnswer("hi"));//wrong
+		//System.out.println(multipleChoice.checkAnswer("1"));//wrong
+		//System.out.println(multipleChoice.checkAnswer("3"));//right
 	}
 
 }
--- src/studio8/Question.java
+++ src/studio8/Question.java
@@ -6,14 +6,15 @@ public class Question {
 	private int points;
 	
 	public Question(String prompt, String answer, int points) {
-		this.prompt = prompt;
+		this.setPrompt(prompt);
 		this.answer = answer;
 		this.points = points;
 	}
 	
 	public void displayPrompt() {
-		System.out.println(this.prompt + "(" + this.points + " points)");
+		System.out.println(this.getPrompt() + "(" + this.points + " points)");
 	}
+	
 	
 	public int checkAnswer(String givenAnswer) {
 		if(answer.equals(givenAnswer)) { //String comparison
@@ -36,5 +37,13 @@ public class Question {
 		q.displayPrompt();
 		System.out.println(q.checkAnswer("hi"));
 		System.out.println(q.checkAnswer("8"));
+	}
+
+	public String getPrompt() {
+		return prompt;
+	}
+
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
 	}
 }
